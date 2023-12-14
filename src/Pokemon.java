75bc98e5785ebf1695423869c1b9ee26d55baee9
Samboduy <samdanielsson000@gmail.com>
2023-12-14 11:31:32 +0100

    sparar json objct och values

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -3,37 +3,57 @@ import com.eclipsesource.json.JsonValue;
 
 import java.util.Random;
 
-public class Pokemon{
+public class Pokemon {
     public Random getRand() {
         return RAND;
     }
-    private final Random RAND = new Random();
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
+    public JsonValue getJSInformation() {
+        return jsInformation;
+    }
+    public void setJSInformation(JsonValue information) {
+        this.jsInformation = information;
+    }
 
+    public JsonObject getJsObject() {
+        return jsObject;
+    }
+
+    public void setJsObject(JsonObject jsObject) {
+        this.jsObject = jsObject;
+    }
+
+    private JsonObject jsObject;
+    private final Random RAND = new Random();
     private String name;
-Pokemon(){
-    // exempel på hur man kan använda Jsonvalue nu med id
-   JsonValue jv = PokeAPI.getRequest(randomId());
-    JsonObject jo = jv.asObject().get("species").asObject();
-    String s = jo.get("name").asString();
-    setName(s);
-    System.out.println(getName());
+    private JsonValue jsInformation;
 
-}
 
-public int randomId(){
+    Pokemon() {
+        // exempel på hur man kan använda Jsonvalue nu med id
+        JsonValue jv = PokeAPI.getRequest(randomId());
+        //sparar jsvalue för framtid manipulation, samma med jsonObject
+        setJSInformation(jv);
+        setJsObject(getJSInformation().asObject());
 
-    int id = getRand().nextInt(1292);
-    id++;
-    return id;
+        JsonObject jo = getJsObject().get("species").asObject();
+        String name = jo.get("name").asString();
+        setName(name);
+        System.out.println(getName());
 
-}
+    }
+
+    public int randomId() {
+
+        int id = getRand().nextInt(1292);
+        id++;
+        return id;
+
+    }
 
 }
